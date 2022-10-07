9787c25b6a7afde1600005803ee637409adbc5a8
unknown <jameskagunga2022@gmail.com>
2022-10-07 06:54:51 -0700

    fixed google map

--- jcargo/ios/Runner/AppDelegate.swift
+++ jcargo/ios/Runner/AppDelegate.swift
@@ -7,7 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyCTmVHTqmMqYwglOHEpKV2S_QUKtzQAA2k")
+    GMSServices.provideAPIKey("AIzaSyBxfJtkWeY1lsKmnfdHdZ0ya-WHMxsZKfU")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
