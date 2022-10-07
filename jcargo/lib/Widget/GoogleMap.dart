53e063f3bff26729d9df004884cbe52ea6ee3264
unknown <jameskagunga2022@gmail.com>
2022-10-07 05:07:40 -0700

    Working on the google map

--- jcargo/lib/Widget/GoogleMap.dart
+++ jcargo/lib/Widget/GoogleMap.dart
@@ -1,9 +1,9 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:jcargo/components/constants.dart';
+import 'package:location/location.dart';
 
 class GoogleMapContainer extends StatefulWidget {
   const GoogleMapContainer({super.key});
@@ -13,33 +13,86 @@ class GoogleMapContainer extends StatefulWidget {
 }
 
 class _GoogleMapContainerState extends State<GoogleMapContainer> {
-  Completer<GoogleMapController> _controller = Completer();
+  final Completer<GoogleMapController> _controller = Completer();
 
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
+  static const LatLng sourceLocation = LatLng(37.33500926, -122.03272188);
+  static const LatLng destinationLocation = LatLng(37.33429383, -122.06600055);
 
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+  List<LatLng> polylinesCoordinates = [];
+  LocationData? currentLocation;
+
+  BitmapDescriptor sourceIcon = BitmapDescriptor.defaultMarker;
+  BitmapDescriptor destinationIcon = BitmapDescriptor.defaultMarker;
+  BitmapDescriptor currentLocationIcon = BitmapDescriptor.defaultMarker;
+
+  void getCurrentLocation() async {
+    Location location = Location();
+
+    location.getLocation().then(
+      (location) {
+        currentLocation = location;
+      },
+    );
+
+    GoogleMapController googleMapController = await _controller.future;
+
+    location.onLocationChanged.listen((newLocation) {
+      currentLocation = newLocation;
+
+      googleMapController.animateCamera(CameraUpdate.newCameraPosition(
+        CameraPosition(
+            zoom: 13.5,
+            target: LatLng(newLocation.latitude!, newLocation.longitude!)),
+      ));
+
+      setState(() {});
+    });
+  }
+
+  void getPolyPoints() async {
+    PolylinePoints polylinePoints = PolylinePoints();
+
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+        google_api_key,
+        PointLatLng(sourceLocation.latitude, sourceLocation.longitude),
+        PointLatLng(
+            destinationLocation.latitude, destinationLocation.longitude));
+
+    if (result.points.isNotEmpty) {
+      result.points.forEach((PointLatLng point) =>
+          polylinesCoordinates.add(LatLng(point.latitude, point.longitude)));
+
+      setState(() {});
+    }
+  }
+
+  @override
+  void initState() {
+    getPolyPoints();
+    getCurrentLocation();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
+        body: GoogleMap(
+      initialCameraPosition: CameraPosition(
+        target: LatLng(sourceLocation.latitude, sourceLocation.longitude),
+        zoom: 14.5,
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {},
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
-      ),
-    );
+      polylines: {
+        Polyline(
+            polylineId: const PolylineId('route'), points: polylinesCoordinates)
+      },
+      markers: {
+        const Marker(markerId: MarkerId("Source"), position: sourceLocation),
+        const Marker(
+            markerId: MarkerId("Destination"), position: destinationLocation),
+      },
+      onMapCreated: (mapController) {
+        _controller.complete(mapController);
+      },
+    ));
   }
 }
