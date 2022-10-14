a6569242e60ad6375ca4cdc19ee3557be88ee7c8
unknown <jameskagunga2022@gmail.com>
2022-10-14 06:53:01 -0700

    working on image assets

--- jcargo/lib/Model/slide.dart
+++ jcargo/lib/Model/slide.dart
@@ -12,21 +12,19 @@ class Slide {
 
 final slideList = [
   Slide(
-    imageUrl:
-        'https://storage.googleapis.com/gweb-uniblog-publish-prod/images/Maps_Pin_FullColor.max-1000x1000.png',
+    imageUrl: 'assets/images/googlemap.png',
     title: 'A Cool Way to Get Start',
     description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dapibus tincidunt bibendum. ',
   ),
   Slide(
-    imageUrl: 'https://images.wsj.net/im-613589/?width=1278&size=1',
+    imageUrl: 'assets/images/travel.png',
     title: 'Design Interactive App UI',
     description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dapibus tincidunt bibendum.',
   ),
   Slide(
-    imageUrl:
-        'https://media.istockphoto.com/photos/friends-ride-sharing-picture-id1143478858?k=20&m=1143478858&s=612x612&w=0&h=ApfGEjH_5hW4uNL89FmWkK_YoR3Ev0RSdVuJdkbxqa4=',
+    imageUrl: 'assets/images/travel3.png',
     title: 'It\'s Just the Beginning',
     description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dapibus tincidunt bibendum. ',
